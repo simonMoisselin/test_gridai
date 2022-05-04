3d122b97e0a63fd862fcc2fc55cac9909d789dcf
Simon Moisselin <simon.moisselin@noble.ai>
2022-05-04 09:40:51 -0700

    wip

--- run_ABC.py
+++ run_ABC.py
@@ -5,12 +5,12 @@ if __name__ == "__main__":
     from argparse import ArgumentParser
 
     print("Start training..")
-    
+
     parser = ArgumentParser()
     parser.add_argument("--steps", type=int, default=100)
     parser.add_argument("--bs", type=int, default=16)
     args = parser.parse_args()
     os.system(
-        f"lightweight-gan --data /datastores/pokemon-jpg --num-train-steps {args.steps} --batch-size {}"
+        f"lightweight-gan --data /datastores/pokemon-jpg --num-train-steps {args.steps} --batch-size {args.bs}"
     )
     print("Success!")
