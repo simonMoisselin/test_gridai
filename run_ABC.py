92c80382dc07ac44015adedc933cdedde349a084
Simon Moisselin <simon.moisselin@noble.ai>
2022-05-03 17:42:59 -0700

    adding requirements and script file

---
+++ run_ABC.py
@@ -0,0 +1,16 @@
+import os
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+
+    print("Start training..")
+    
+    parser = ArgumentParser()
+    parser.add_argument("--steps", type=int, default=100)
+    parser.add_argument("--bs", type=int, default=16)
+    args = parser.parse_args()
+    os.system(
+        f"lightweight-gan --data /datastores/pokemon-jpg --num-train-steps {args.steps} --batch-size {}"
+    )
+    print("Success!")
